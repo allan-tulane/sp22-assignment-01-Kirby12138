c336f1f1c94ff5379d06766c85ff74dc0a32c759
Kirby12138 <82625392+Kirby12138@users.noreply.github.com>
2022-02-18 23:57:46 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def longest_run(mylist, key):
     return longest
 # work = O(n)
 # span = O(n)
-        
+
 
 
 class Result:
@@ -39,28 +39,63 @@ class Result:
         self.right_size = right_size             # run on right side of input
         self.longest_size = longest_size         # longest run in input
         self.is_entire_range = is_entire_range   # True if the entire input matches the key
-        
+
     def __repr__(self):
         return('longest_size=%d left_size=%d right_size=%d is_entire_range=%s' %
               (self.longest_size, self.left_size, self.right_size, self.is_entire_range))
-    
-    
+
+
 def longest_run_recursive(mylist, key):
+    res = recursion(mylist,key)
+    return res.longest_size
     ### TODO
-    if len(mylist) == 1 and mylist[0] == key:
-        return Result(1, 1, 1, True)
+def recursion(mylist,key):
+    if len(mylist) == 1:
+        if mylist[0] == key:
+            return Result(1, 1, 1, True)
+        else:
+            return Result(0,0,0,False)
+    left = recursion(mylist[0 :len(mylist)//2], key)
+    right = recursion(mylist[len(mylist)//2: ], key)
+    print(left,right)
+    print(mylist[ :len(mylist)//2],mylist[len(mylist)//2: ])
+    if left.is_entire_range and right.is_entire_range:
+        temp = left.right_size + right.left_size
+        return Result(temp, temp, temp, True)
+
+    elif left.is_entire_range:
+        if right.left_size > 0:
+            temp = left.right_size + right.left_size
+            return Result(temp,right.right_size,temp,False)
+        else:
+            return Result(left.left_size,right.right_size,max(left.left_size,right.longest_size),False)
+
+    elif right.is_entire_range:
+        if left.right_size > 0:
+            temp = left.right_size + right.left_size
+            return Result(left.left_size,temp,temp,False)
+        else:
+            return Result(left.left_size,right.right_size,max(right.right_size,left.longest_size),False)
+
     else:
-        return Result(0,0,0,False)
-    left = longest_run_recursive(mylist[ :len(mylist)//2], key)
-    right = longest_run_recursive(mylist[len(mylist)//2: ], key)
-    return 
-    
-        
+        if left.right_size > 0 and right.left_size > 0:
+            temp = left.right_size + right.left_size
+            return Result(left.left_size,right.right_size,max(left.longest_size,right.longest_size,temp),False)
+        elif left.right_size >0:
+            return Result(left.left_size,right.right_size,max(left.longest_size,right.longest_size),False)
+        elif right.left_size >0:
+            return Result(left.left_size,right.right_size,max(left.longest_size,right.longest_size),False)
+        elif left.left_size >0:
+            return Result(left.left_size,0,max(left.longest_size,right.longest_size),False)
+        elif right.right_size>0:
+            return Result(0,right.right_size,max(left.longest_size,right.longest_size),False)
+        else:
+            return Result(0,0,0,False)
+
+
 
 ## Feel free to add your own tests here.
 def test_longest_run():
-    return longest_run([2,12,12,8,12,12,12,0,12,1,1], 12)
+    return longest_run([2,12,12,8,12,12,12,0,12,1], 999)
 
-print(foo(10))
 print(test_longest_run())
-
